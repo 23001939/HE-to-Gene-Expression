5ab3f74635b77c18bec3fb91b0fcd5c920173366
Thang <23001939@hus.edu.vn>
2026-08-01 12:42:36 +0700

    Kiến trúc mo hình LightHGGEP

--- run_pipeline.py
+++ run_pipeline.py
@@ -355,7 +355,8 @@ trainer = pl.Trainer(
     callbacks=[early_stop_callback, checkpoint_callback],
     logger=default_logger,
     log_every_n_steps=10,
-    gradient_clip_val=1.0
+    gradient_clip_val=1.0,
+    use_distributed_sampler=False  # Thêm dòng này để báo Lightning không tự động inject sampler
 )
 
 # Train
