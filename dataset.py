d3450625144d75c392d66e8db0bf827a4490b7cb
23001939 <23001939@hus.edu.vn>
2026-09-20 10:07:37 +0700

    Remove get_test_labels method from dataset.py
    Removed the get_test_labels method that concatenated ground-truth labels for all slices.

--- dataset.py
+++ dataset.py
@@ -199,22 +199,6 @@ class HER2ST(torch.utils.data.Dataset):
         df.set_index('id',inplace=True)
         return df
 
-    def get_test_labels(self):
-        """[MỚI - sửa lỗi ARI/NMI đa lát cắt] Nối nhãn ground-truth của TẤT CẢ lát cắt
-        trong self.names theo đúng thứ tự, dùng 'undetermined' cho lát cắt không có
-        annotation. Cùng thứ tự với adata_pred/adata_gt được nối trong stnet_predict /
-        histogene_predict (torch.cat theo đúng thứ tự self.names, shuffle=False).
-        """
-        parts = []
-        for name in self.names:
-            lab = self.label.get(name)
-            if lab is None:
-                lab = np.full(len(self.meta_dict[name]), 'undetermined')
-            parts.append(np.asarray(lab))
-        if not parts:
-            return None
-        return np.concatenate(parts)
-
     
     def get_meta(self,name,gene_list=None):
         cnt = self.get_cnt(name)
